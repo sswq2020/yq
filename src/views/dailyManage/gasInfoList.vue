e49ac3bc327ba8cebe8df989d70f7ca2618e0767
shiswq@hletong.com <shiswq@hletong.com>
2019-09-02 10:20:42 +0800

    feat: 🎸 加注明细在table上添加单页和总页的数据

--- src/views/dailyManage/gasInfoList.vue
+++ src/views/dailyManage/gasInfoList.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container single-page">
-    <hlBreadcrumb :data="breadTitle">
-    </hlBreadcrumb>
+    <hlBreadcrumb :data="breadTitle"></hlBreadcrumb>
     <div class="search-box">
       <div class="form-item">
         <label>加注日期</label>
         <div class="form-control">
-          <el-date-picker format="yyyy-MM" value-format="yyyy-MM" v-model="form.createdTime" type="month" placeholder="选择月"></el-date-picker>
+          <el-date-picker
+            format="yyyy-MM"
+            value-format="yyyy-MM"
+            v-model="form.createdTime"
+            type="month"
+            placeholder="选择月"
+          ></el-date-picker>
         </div>
       </div>
       <div class="form-item" v-if="!IS_OILVIP">
@@ -29,6 +34,44 @@
           size="small"
         >查询</el-button>
         <el-button size="small" @click="clearListParams">重置</el-button>
+      </div>
+    </div>
+    <div class="table-title">
+      <div class="table-title-row">
+        <div>
+          单页加油量小计
+          <span class="info">{{listData.currOil || 0}}</span>L
+        </div>
+        <div>
+          单页加油金额小计￥
+          <span class="info">{{listData.currOilMoney || 0}}</span>
+        </div>
+        <div>
+          总页加油量合计
+          <span class="info">{{listData.totleOil || 0}}</span>L
+        </div>
+        <div>
+          总页加油金额合计￥
+          <span class="info">{{listData.totleOilMoney || 0}}</span>
+        </div>
+      </div>
+      <div class="table-title-row">
+        <div>
+          单页加气量小计
+          <span class="info">{{listData.currAir || 0}}</span>kg
+        </div>
+        <div>
+          单页加气金额小计￥
+          <span class="info">{{listData.currAirMoney || 0}}</span>
+        </div>
+        <div>
+          总页加气量合计
+          <span class="info">{{listData.totleAir || 0}}</span>kg
+        </div>
+        <div>
+          总页加气金额合计￥
+          <span class="info">{{listData.totleAirMoney || 0}}</span>
+        </div>
       </div>
     </div>
     <heltable
@@ -62,13 +105,13 @@ import { mapMutations, mapGetters } from "vuex";
 import Dict from "util/dict.js";
 import heltable from "components/hl_table";
 import hlBreadcrumb from "components/hl-breadcrumb";
-import moment from "moment"
+import moment from "moment";
 import gsStationglass from "components/gsStationglass";
 
 const defaultFormData = {
   createdTime: moment().format("YYYY-MM"),
   gsId: null,
-  gsName:null,
+  gsName: null,
   userPlate: null
 };
 const defaultListParams = {
@@ -76,6 +119,14 @@ const defaultListParams = {
   page: 1
 };
 const defaultListData = {
+  currAir: 0,
+  currAirMoney: 0,
+  currOil: 0,
+  currOilMoney: 0,
+  totleAir: 0,
+  totleAirMoney: 0,
+  totleOil: 0,
+  totleOilMoney: 0,
   paginator: {
     totalCount: 0,
     totalPage: 1
@@ -177,7 +228,7 @@ export default {
       this.listParams = { ...defaultListParams };
       this.getListData();
     },
-    getGs(obj){
+    getGs(obj) {
       this.form.gsId = obj.gsId;
       this.form.gsName = obj.gsName;
     },
@@ -223,6 +274,26 @@ export default {
     padding-top: 20px;
     .el-button {
       margin-top: 17px;
+    }
+  }
+}
+
+.table-title {
+  position: relative;
+  top: 1px;
+  padding: 16px 0 0 0;
+  box-sizing: border-box;
+  .table-title-row {
+    line-height: 1.5;
+    color: rgba(0, 0, 0, 0.65);
+    font-size: 14px;
+    display: flex;
+    justify-content: space-around;
+    width: 80%;
+    margin: 0px auto;
+    .info {
+      color: red;
+      font-size: 1rem;
     }
   }
 }
