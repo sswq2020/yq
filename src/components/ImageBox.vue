a2ed2b63ee1a20bbb5d502287364a0bc54c83def
shiswq@hletong.com <shiswq@hletong.com>
2019-11-25 16:24:33 +0800

    feat: 🎸 图片显示组件改为并排

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -59,12 +59,14 @@ export default {
 <style  scoped lang="less">
 .imgBox {
   box-sizing: border-box;
+  display: inline-block;
   width: 90px;
   height: 90px;
   padding: 8px;
   border-radius: 4px;
   border: 1px solid hsl(0, 0%, 85%);
   position: relative;
+  margin-right: 5px;
   .img {
     width: 100%;
     height: 100%;
